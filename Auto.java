d96f6a151603a23b1ca20821c037f73a55278db7
Maliha654 <amatul.shakur.mohammed19@gmail.com>
2025-11-04 16:18:51 -0700

    Update Auto.java

--- Auto.java
+++ Auto.java
@@ -1,1 +1,6 @@
-I
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
