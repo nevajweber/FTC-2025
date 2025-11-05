666cdc29c1ac1243c1f1f33081c054e414c93929
Maliha654 <amatul.shakur.mohammed19@gmail.com>
2025-11-04 17:19:30 -0700

    Update Auto.java

--- Auto.java
+++ Auto.java
@@ -1,6 +1,11 @@
-package org.firstinspires.ftc.teamcode.auton;
+package org.firstinspires.ftc.teamcode.util;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class PIDFController {
+  //Gains 
+  public double kP, kI, kD, KF;
+
+  //Limits
+  public double iMin = -1.0, iMax = 1.0;     // integral clamp
+  public double outMin = -1.0, outMax = 1.0;
