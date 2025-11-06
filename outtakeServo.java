dccd173c45de28136365d865efb8af2369c2bacd
nevajweber <nevajweber@gmail.com>
2025-11-06 12:04:59 -0700

    Update outtakeServo.java

--- outtakeServo.java
+++ outtakeServo.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="Servo Test OpMode", group="FTC")
-public class ServoTestOpMode extends LinearOpMode {
+public class DriveTeleOp extends LinearOpMode {
 
     private Servo myServo;
 
